3aeb45e6bae3e6c3e40a891aac1b74c00a24f967
Antonio Cavallo <a.cavallo@cavallinux.eu>
2026-09-05 10:51:24 -0400

    test III

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -24,7 +24,6 @@ def test_build_abort_in_existing_nonempty_dir(project_cli_runner):
     assert result.exit_code == 1
 
 
-@pytest.mark.skip(reason="test")
 def test_build_continue_in_existing_nonempty_dir(project_cli_runner):
     os.mkdir("build_dir")
     with open("build_dir/test", "w", encoding="utf-8"):
@@ -34,28 +33,24 @@ def test_build_continue_in_existing_nonempty_dir(project_cli_runner):
     assert result.exit_code == 0
 
 
-@pytest.mark.skip(reason="test")
 def test_alias(project_cli_runner):
     result = project_cli_runner.invoke(cli, ["pr"])  # short for 'project-info'
     assert result.exit_code == 0
     assert "Name: Demo Project" in result.output
 
 
-@pytest.mark.skip(reason="test")
 def test_dev_cmd_alias(isolated_cli_runner):
     result = isolated_cli_runner.invoke(cli, ["dev", "s"])  # short for 'shell'
     assert result.exit_code == 2
     assert "Error: Could not automatically discover a project" in result.output
 
 
-@pytest.mark.skip(reason="test")
 def test_alias_multiple_matches(project_cli_runner):
     result = project_cli_runner.invoke(cli, ["p"])  # short for 'project-info' & 'plugins'
     assert result.exit_code == 2
     assert "Error: Too many matches" in result.output
 
 
-@pytest.mark.skip(reason="test")
 def test_alias_no_matches(project_cli_runner):
     result = project_cli_runner.invoke(cli, ["z"])
     assert result.exit_code == 2
